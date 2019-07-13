0180108e2900019526a92423fcaa62454c258a61
clayliau-cloud <clayliau@gmail.com>
2019-07-13 22:06:55 +0800

    Finish final project....but I don't have experience in frontend decoration. I use default settings

--- Lesson-4/My-Final-Project/finalproject.py
+++ Lesson-4/My-Final-Project/finalproject.py
@@ -42,12 +42,22 @@ def deleteOnefromDB(itemObj):
     except:
         return NoOK
 
+def queryRestaurantMenufromDB(target_id):
+    res_query = session.query(MenuItem).filter(MenuItem.restaurant_id==target_id)
+    session.remove()
+    return res_query
+
 
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
     restaurants = queryAllfromDB(Restaurant)
     return render_template('restaurants.html', restaurants = restaurants)
+
+@app.route('/restaurants/JSON')
+def showRestaurantsJSON():
+    restaurants = queryAllfromDB(Restaurant)
+    return jsonify(Restaurants = [i.serialize for i in restaurants])
 
 @app.route('/restaurant/new', methods=['POST', 'GET'])
 def newRestaurant():
@@ -73,8 +83,6 @@ def editRestaurant(restaurant_id):
     else:
         return render_template('editRestaurant.html', restaurant=restaurant_query)
 
-    
-
 @app.route('/restaurant/<int:restaurant_id>/delete', methods=['POST', 'GET'])
 def deleteRestaurant(restaurant_id):
     restaurant_query = queryOnefromDB(Restaurant, restaurant_id)
@@ -91,19 +99,60 @@ def deleteRestaurant(restaurant_id):
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-    return "This page is the menu for restaurant %s" % restaurant_id
+    menu_item = queryRestaurantMenufromDB(restaurant_id)
+    restaurant = queryOnefromDB(Restaurant, restaurant_id)
+    return render_template('menu.html', restaurant = restaurant, items = menu_item)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/new')
+@app.route('/restaurant/<int:restaurant_id>/JSON')
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
+def showMenuJSON(restaurant_id):
+    menu_item = queryRestaurantMenufromDB(restaurant_id)
+    return jsonify(MenuItems = [i.serialize for i in menu_item])
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def showMenuItemJSON(restaurant_id, menu_id):
+    target_item = queryOnefromDB(MenuItem, menu_id)
+    return jsonify(MenuItem = target_item.serialize)
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new', methods=['POST', 'GET'])
 def newMenuItem(restaurant_id):
-    return "This page is for adding new menu item for restaurant %s" % restaurant_id
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'],\
+                            description =  request.form['description'],\
+                            price = request.form['price'],\
+                            course = request.form['course'],
+                            restaurant_id = restaurant_id)
+        addEditItemtoDB(newItem)
+        flash('New menu item %s is added in database' %request.form['name'])
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newmenuitem.html')    
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit', methods = ['POST', 'GET'])
 def editMenuItem(restaurant_id, menu_id):
-    return "This page is for editing menu item  %s" % menu_id
+    target_item = queryOnefromDB(MenuItem, menu_id)
+    if request.method == 'POST':
+        old_name = target_item.name
+        target_item.name = request.form['name']
+        target_item.description =  request.form['description']
+        target_item.price = request.form['price']
+        target_item.course = request.form['course']
+        addEditItemtoDB(target_item)
+        flash('Menu item %s is modified in database' %old_name)
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+    else:
+        return  render_template('editmenuitem.html', item = target_item, restaurant_id= restaurant_id)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete', methods = ['POST', 'GET'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "This page is for deleting menu item  %s" % menu_id
+    target_item = queryOnefromDB(MenuItem, menu_id)
+    if request.method == 'POST':
+        deleteOnefromDB(target_item)
+        flash('Menu item %s is deleted in database' %target_item.name)
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+    else:
+        return  render_template('deletemenuitem.html', item = target_item, restaurant_id= restaurant_id)
+
 
 if __name__=='__main__':
     app.secret_key = 'super_secret_key'
